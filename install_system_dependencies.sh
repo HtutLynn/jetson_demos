4a629d2f404c37e4328bca1d0c94e38389b181af
Htut Lynn Aung <htutlynnaung.cs@gmail.com>
2020-10-20 10:42:00 +0630

    Protobuf compiler installation is fixed#2.

--- install_system_dependencies.sh
+++ install_system_dependencies.sh
@@ -26,7 +26,7 @@ folder=${HOME}/src
 mkdir -p $folder
 
 echo "** Install requirements"
-sudo apt-get install -y autoconf libtool
+apt-get install -y autoconf libtool
 
 echo "** Download protobuf-3.8.0 sources"
 cd $folder
@@ -42,7 +42,7 @@ fi
 echo "** Install protoc"
 unzip protobuf-python-3.8.0.zip
 unzip protoc-3.8.0-linux-aarch_64.zip -d protoc-3.8.0
-sudo cp protoc-3.8.0/bin/protoc /usr/local/bin/protoc
+cp protoc-3.8.0/bin/protoc /usr/local/bin/protoc
 
 echo "** Build and install protobuf-3.8.0 libraries"
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
@@ -51,16 +51,16 @@ cd protobuf-3.8.0/
 ./configure --prefix=/usr/local
 make -j$(nproc)
 make check
-sudo make install
-sudo ldconfig
+make install
+ldconfig
 
 echo "** Update python3 protobuf module"
 # remove previous installation of python3 protobuf module
-sudo pip3 uninstall -y protobuf
-sudo pip3 install Cython
+pip3 uninstall -y protobuf
+pip3 install Cython
 cd python/
 python3 setup.py build --cpp_implementation
 python3 setup.py test --cpp_implementation
-sudo python3 setup.py install --cpp_implementation
+python3 setup.py install --cpp_implementation
 
 echo "** Build protobuf-3.8.0 successfully"
