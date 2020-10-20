52a9aa9c0b60701421e7ddd2ddaafe435d58c497
Htut Lynn Aung <htutlynnaung.cs@gmail.com>
2020-10-20 11:23:45 +0630

    Protobuf compiler installation is fixed#3.

--- install_system_dependencies.sh
+++ install_system_dependencies.sh
@@ -28,6 +28,74 @@ mkdir -p $folder
 echo "** Install requirements"
 apt-get install -y autoconf libtool
 
+echo "** Install autoconf from source"
+wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz -q --show-progress --no-clobber
+tar xvfz autoconf-2.69.tar.gz
+cd autoconf-2.69
+./configure --prefix=/usr/local/autoconf/2_69
+make
+make install
+
+echo "** create symbolic link for autoconf..."
+ln -s /usr/local/autoconf/2_69/bin/autoconf /usr/local/bin/
+ln -s /usr/local/autoconf/2_69/bin/autoheader /usr/local/bin/
+ln -s /usr/local/autoconf/2_69/bin/autom4te /usr/local/bin/
+ln -s /usr/local/autoconf/2_69/bin/autoreconf /usr/local/bin/
+ln -s /usr/local/autoconf/2_69/bin/autoscan /usr/local/bin/
+ln -s /usr/local/autoconf/2_69/bin/autoupdate /usr/local/bin/
+ln -s /usr/local/autoconf/2_69/bin/ifnames /usr/local/bin/
+
+echo "** set Environment Variable..."
+echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH" >> ~/.bashrc
+source ~/.bashrc
+
+echo "autoconf installation done!"
+# echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf 
+# ldconfig
+# ldconfig -v
+
+echo "** Install libtool from source"
+wget http://ftp.jaist.ac.jp/pub/GNU/libtool/libtool-2.4.6.tar.gz -q --show-progress --no-clobber
+tar xvfz libtool-2.4.6.tar.gz
+cd libtool-2.4.6
+./configure --prefix=/usr/local/libtool/2_4_6
+make
+make install
+
+# /usr/local/include
+echo "** create symbolic link for libtool"
+# /usr/local/bin
+ln -s /usr/local/libtool/2_4_6/bin/libtool /usr/local/bin/
+ln -s /usr/local/libtool/2_4_6/bin/libtoolize /usr/local/bin/
+ln -s /usr/local/libtool/2_4_6/include/libltdl /usr/local/include/
+ln -s /usr/local/libtool/2_4_6/include/ltdl.h /usr/local/include/
+
+# /usr/local/lib
+ln -s /usr/local/libtool/2_4_6/lib/libltdl.a /usr/local/lib/
+ln -s /usr/local/libtool/2_4_6/lib/libltdl.la /usr/local/lib/
+ln -s /usr/local/libtool/2_4_6/lib/libltdl.so /usr/local/lib/
+ln -s /usr/local/libtool/2_4_6/lib/libltdl.so.7 /usr/local/lib/
+ln -s /usr/local/libtool/2_4_6/lib/libltdl.so.7.3.1 /usr/local/lib/
+
+# /usr/local/share/aclocal
+ln -s /usr/local/libtool/2_4_6/share/aclocal/libtool.m4 /usr/local/share/aclocal/
+ln -s /usr/local/libtool/2_4_6/share/aclocal/ltargz.m4 /usr/local/share/aclocal/
+ln -s /usr/local/libtool/2_4_6/share/aclocal/ltdl.m4 /usr/local/share/aclocal/
+ln -s /usr/local/libtool/2_4_6/share/aclocal/lt~obsolete.m4 /usr/local/share/aclocal/
+ln -s /usr/local/libtool/2_4_6/share/aclocal/ltoptions.m4 /usr/local/share/aclocal/
+ln -s /usr/local/libtool/2_4_6/share/aclocal/ltsugar.m4 /usr/local/share/aclocal/
+ln -s /usr/local/libtool/2_4_6/share/aclocal/ltversion.m4 /usr/local/share/aclocal/
+
+echo "** set environment variable for libtool"
+echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" > ~/.bashrc
+echo "PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH" > ~/.bashrc
+echo "ACLOCAL_PATH=/usr/local/share/aclocal:$ACLOCAL_PATH" > ~/.bashrc
+source ~/.bashrc
+
+echo "libool installation done!"
+
+
 echo "** Download protobuf-3.8.0 sources"
 cd $folder
 if [ ! -f protobuf-python-3.8.0.zip ]; then
