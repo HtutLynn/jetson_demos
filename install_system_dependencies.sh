a2104e5cf7a887ff858baf91468e1535fb3438cb
Htut Lynn Aung <htutlynnaung.cs@gmail.com>
2020-10-20 10:33:25 +0630

    Protobuf compiler installation is fixed.

--- install_system_dependencies.sh
+++ install_system_dependencies.sh
@@ -21,7 +21,46 @@ echo "Exporting CMake path..."
 echo 'export PATH=/nvdli-nano/jetson_demos/cmake-3.13.0/bin/:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
-apt-get install libfreetype6-dev python3-setuptools
-apt-get install protobuf-compiler libprotobuf-dev openssl
-apt-get install libssl-dev libcurl4-openssl-dev
-apt-get install cython3
+echo "Installing protobuf compiler..."
+folder=${HOME}/src
+mkdir -p $folder
+
+echo "** Install requirements"
+sudo apt-get install -y autoconf libtool
+
+echo "** Download protobuf-3.8.0 sources"
+cd $folder
+if [ ! -f protobuf-python-3.8.0.zip ]; then
+  echo "Downloading protobuf python..."
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v3.8.0/protobuf-python-3.8.0.zip
+fi
+if [ ! -f protoc-3.8.0-linux-aarch_64.zip ]; then
+  echo "Downloading protobuf-linux-aarch_64"
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v3.8.0/protoc-3.8.0-linux-aarch_64.zip
+fi
+
+echo "** Install protoc"
+unzip protobuf-python-3.8.0.zip
+unzip protoc-3.8.0-linux-aarch_64.zip -d protoc-3.8.0
+sudo cp protoc-3.8.0/bin/protoc /usr/local/bin/protoc
+
+echo "** Build and install protobuf-3.8.0 libraries"
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+cd protobuf-3.8.0/
+./autogen.sh
+./configure --prefix=/usr/local
+make -j$(nproc)
+make check
+sudo make install
+sudo ldconfig
+
+echo "** Update python3 protobuf module"
+# remove previous installation of python3 protobuf module
+sudo pip3 uninstall -y protobuf
+sudo pip3 install Cython
+cd python/
+python3 setup.py build --cpp_implementation
+python3 setup.py test --cpp_implementation
+sudo python3 setup.py install --cpp_implementation
+
+echo "** Build protobuf-3.8.0 successfully"
